e3fcf2182da694d66eb8a7a625342061cd97737b
aviad <aviad.g@layerxsecurity.com>
2025-06-30 16:03:32 +0300

    wrap fetch-override in IIFE for better encapsulation

--- src/fetch-override.js
+++ src/fetch-override.js
@@ -1,18 +1,17 @@
-console.log("fetch-override.js is injected");
-const originalFetch = window.fetch;
-window.fetch = async (input, options = {}) => {
-  try {
-    const url = input.url || input.toString();
-    console.log("url", url);
-    if(Object.keys(options).length > 0) {
-      console.log("options: ", options);
+(function () {
+  console.log("fetch-override.js is injected");
+  const originalFetch = window.fetch;
+  window.fetch = async (input, options = {}) => {
+    try {
+      const url = input.url || input.toString();
+      console.log("url", url);
+      if (Object.keys(options).length > 0) {
+        console.log("options: ", options);
+      }
+      return originalFetch.call(this, input, options);
+    } catch (e) {
+      console.error("error during fetch", e);
+      return originalFetch.call(this, input, options);
     }
-    return originalFetch.call(this, input, options);
-  } catch (e) {
-    console.error("error during fetch", e);
-    return originalFetch.call(this, input, options);
-  }
-};
-
-
-
+  };
+})();
